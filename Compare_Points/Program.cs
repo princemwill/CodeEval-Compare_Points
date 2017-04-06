c2c53d6900f569f32057b70df8c77d20100b36b6
princemwill <princemwilliams@gmail.com>
2017-04-06 15:55:59 -0400

    almost there. needs refactoring.

--- Compare_Points/Program.cs
+++ Compare_Points/Program.cs
@@ -19,8 +19,63 @@ namespace Compare_Points
                         continue;
                                         
                     string[] coordinates = line.Split();
-                    Console.WriteLine(coordinates[0]);
+                    //Console.WriteLine(coordinates[0]);
 
+                    //first set of coordinates "x,y" named o & p in challenge
+                    int o = int.Parse(coordinates[0]);
+                    int p = int.Parse(coordinates[1]);
+
+                    //second set of coordinates "x2,y2" named q & r in challenge
+                    int q = int.Parse(coordinates[2]);
+                    int r = int.Parse(coordinates[3]);
+
+                    
+
+
+                    var location = new StringBuilder();
+                    
+                        if ((p - r == 0) && (o - q == 0))
+                        {
+                            location.Append("here");                           
+                        }
+                        else if (p - r < 0)
+                        {
+                            location.Append("N");
+                        }
+                        else if (p - r > 0)
+                        {
+                            location.Append("S");
+                        }
+                        else if (p - r == 0)
+                        {
+                            location.Append("");
+                        }
+                        else if (o - q < 0)
+                        {
+                            location.Append("E");
+                        }
+                        else if (o - q > 0)
+                        {
+                            location.Append("W");
+                        }
+                        else if (o - q == 0)
+                        {
+                            location.Append("");
+                        }
+                        if (p - r < 0)
+                        {
+                            location.Append("E");
+                        }
+                        else if (p - r > 0)
+                        {
+                            location.Append("W");
+                        }
+                        else if (p - r == 0)
+                        {
+                            location.Append("");
+                        }
+                                        
+                    Console.WriteLine(location);
 
                 }
             Console.ReadKey();
