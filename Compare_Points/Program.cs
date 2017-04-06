4ef86949a7ddf23832793a28502d7f3f426726be
princemwill <princemwilliams@gmail.com>
2017-04-06 14:16:18 -0400

    sample code for reading from file added from codeEval

--- Compare_Points/Program.cs
+++ Compare_Points/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace Compare_Points
     {
         static void Main(string[] args)
         {
+            using (StreamReader reader = File.OpenText(@"C:\Users\princ\Documents\Notes\CodeEval_ComparePoints.txt"))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    // do something with line
+                }
+
         }
     }
 }
